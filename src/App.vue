a6de838f7151b597c05cfbdfb1f43d0cf49d6488
junichiseki0831 <soulfly_0831@yahoo.co.jp>
2021-08-09 19:37:48 +0900

    全体のcssファイルを追加

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
-     <board />
+    <!-- Boardコンポーネントをレンダリング -->
+    <board />
   </div>
 </template>
 
 <script>
+//コンポーネントをインポート
 import Board from './components/Board.vue'
 
 export default {
   name: 'app',
   components: {
+    //コンポーネント登録
     Board
   }
 }
