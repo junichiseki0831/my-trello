b8eb98f6f0b2ec7feb6a2314a1631051e2d49ada
junichiseki0831 <soulfly_0831@yahoo.co.jp>
2021-08-15 18:24:06 +0900

    リスト作成（List.vue）コンポーネント追加

---
+++ src/components/List.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="list">
+    <div class="listheader">
+      <p class="list-title">{{ title }}</p>
+      <div class="deletelist" @click="removeList">×</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    //ListAddコンポーネントよりデータを受け取り
+    title: {
+      type: String,
+      required: true
+    },
+    listIndex: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    //リスト削除メソッド
+    removeList: function() {
+      if (confirm("本当にこのリストを削除しますか？")) {
+        this.$store.dispatch("removelist", { listIndex: this.listIndex });
+      }
+    }
+  }
+};
+</script>
